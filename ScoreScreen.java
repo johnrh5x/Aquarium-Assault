13d724a316fff2ab5df8704c7569c1f4beab0a43
John Harris <johnrh5x@gmail.com>
2022-03-30 08:03:26 -0500

    Updated ScoreScreen to fix spacing and do title animation on win.

--- ScoreScreen.java
+++ ScoreScreen.java
@@ -14,11 +14,21 @@ public class ScoreScreen extends ScreenAdapter implements Constants {
 
 	// Fields
 	
+	private static final String[] TITLES = {"Vitamin H",
+		                                    "Alabaster Titan",
+		                                    "Me, Blorko!",
+		                                    "Sir Anthony Hopkins",
+		                                    "Electricity Cop",
+		                                    "Tobias Beckford"};
+		                                  
+	private static final int[] CUTOFFS = {1667,1333,1000,667,333,0};
+	
 	private AquariumAssault game;
 	private Stage           stage;
 	private TextActor[]     line;
 	private int             index;
-	private boolean         inputFlag;
+	private boolean         inputFlag = false;
+	private float           elapsedTime = 0f;
 	
 	// Constructor
 	
@@ -47,41 +57,33 @@ public class ScoreScreen extends ScreenAdapter implements Constants {
 			line[1] = new TextActor(game.font(),game.fontShader(),"You have been sacked.");
 			line[2] = new TextActor(game.font(),game.fontShader(),"Cast out of the aquarium,");
 			line[3] = new TextActor(game.font(),game.fontShader(),"you spend the rest of your life");
-			line[4] = new TextActor(game.font(),game.fontShader(),"writing for obscure financial trade publications.");
-			float dy = WORLD_HEIGHT/(line.length + 1f);
+			line[4] = new TextActor(game.font(),game.fontShader(),"writing for obscure financial trade publications.");			
 			for (int i = 0; i < line.length; i++) {
-				line[i].setSize(WORLD_WIDTH,GRID_STEP);
-				line[i].setScale(0.5f);
-				line[i].setColor(Color.BLACK);
-				line[i].setPosition(0f,WORLD_HEIGHT - (i + 1)*dy - 0.5f*line[i].getHeight());
+				line[i].setColor(Color.RED);
 				line[i].toggleTyping();
 			}
+			stage.addActor(line[0]);
+			index = 0;
 		} else {
-			line = new TextActor[2];
-			line[0] = new TextActor(game.font(),game.fontShader(),"Your score is " + game.getScore() + ",");
-			String title = "Vitamin H";
-			if (game.getScore() < 0) {
-				title = "Tobias Beckford";
-			} else if (game.getScore() < 500) {
-				title = "Electricity Cop";
-			} else if (game.getScore() < 1000) {
-				title = "Sir Antony Hopkins";
-			} else if (game.getScore() < 1500) {
-				title = "Alabaster  Titan";
+			line = new TextActor[3 + TITLES.length];
+			line[0] = new TextActor(game.font(),game.fontShader(),"Your score of");
+			line[1] = new TextActor(game.font(),game.fontShader(),String.valueOf(game.getScore()));
+			line[2] = new TextActor(game.font(),game.fontShader(),"earns you the title of:");
+			for (int i = 0; i < 3; i++) line[i].setColor(Color.RED);
+			for (int i = 3; i < line.length; i++) {
+				line[i] = new TextActor(game.font(),game.fontShader(),TITLES[i-3]);
+				line[i].setColor(Color.BLUE);
 			}
-			line[1] = new TextActor(game.font(),game.fontShader(),"which earns you the title '" + title + "'.");
-			for (int i = 0; i < line.length; i++) {
-				line[i].setSize(WORLD_WIDTH,GRID_STEP);
-				line[i].setScale(0.5f);
-				line[i].setColor(Color.BLACK);
-				line[i].toggleTyping();
-			}
-			line[0].setPosition(0f,0.5f*WORLD_HEIGHT);
-			line[1].setPosition(0f,0.5f*WORLD_HEIGHT - line[1].getHeight());
+			index = TITLES.length - 1;
+			line[index + 3].setColor(Color.RED);
+			for (int i = 0; i < line.length; i++) stage.addActor(line[i]);
 		}
-		index = 0;
-		stage.addActor(line[0]);
-		inputFlag = false;
+		float dy = WORLD_HEIGHT/(line.length + 1f);
+		for (int i = 0; i < line.length; i++) {
+			line[i].setSize(WORLD_WIDTH,GRID_STEP);
+			line[i].setScale(0.5f);
+			line[i].setPosition(0f, WORLD_HEIGHT - (i + 1)*dy - 0.5f*GRID_STEP);
+		}
 		
 	}
 	
@@ -98,9 +100,15 @@ public class ScoreScreen extends ScreenAdapter implements Constants {
 			game.setScreen(new CreditsScreen(game));
 		} else {
 			inputFlag = true;
-			for (TextActor t: line) {
-				t.toggleTyping();
-				if (t.getStage() == null) stage.addActor(t);
+			if (game.lostDogfish()) {
+				for (TextActor t: line) {
+					t.toggleTyping();
+					if (t.getStage() == null) stage.addActor(t);
+				}
+			} else {
+				line[index + 3].setColor(Color.BLUE);
+				while (game.getScore() < CUTOFFS[index] && index > 0) index--;
+				line[index + 3].setColor(Color.RED);
 			}
 		}
 		
@@ -110,11 +118,24 @@ public class ScoreScreen extends ScreenAdapter implements Constants {
 	public void render(float delta) {
 		
 		stage.act(delta);
-		if (line[index].finishedTyping()) {
-			if (index == line.length - 1) {
-				inputFlag = true;
-			} else {
-				stage.addActor(line[++index]);
+		if (game.lostDogfish()) {
+			if (line[index].finishedTyping()) {
+				if (index == line.length - 1) {
+					inputFlag = true;
+				} else {
+					stage.addActor(line[++index]);
+				}
+			}
+		} else {
+			if (!inputFlag) {
+				elapsedTime += delta;
+				if (elapsedTime > 0.5f && game.getScore() > CUTOFFS[index]) {
+					line[index + 3].setColor(Color.BLUE);
+					index--;
+					line[index + 3].setColor(Color.RED);
+					elapsedTime = 0f;
+					inputFlag = game.getScore() <= CUTOFFS[index] || index == 0;
+				}
 			}
 		}
 		Gdx.gl.glClearColor(1f,1f,1f,1f);
